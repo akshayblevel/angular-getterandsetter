d2366d35a0e6a498933e56d9e5c9adb6c021f9c2
akshayblevel <akshayblevel@gmail.com>
2021-08-21 17:54:09 +0530

    3 files modified, 3 added, 1 deleted

---
+++ src/app/employee-component/employee-component.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-component',
+  templateUrl: './employee-component.component.html',
+  styleUrls: ['./employee-component.component.css']
+})
+export class EmployeeComponentComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+
+  private _listFilter: string = '';
+
+  get listFilter(): string {
+    return this._listFilter;
+  }
+
+  set listFilter(value: string) {
+    this._listFilter = value;
+    console.log('In Setter:', value);
+  }
+}
